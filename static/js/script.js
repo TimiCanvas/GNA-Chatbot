e7bd07f88cf87ea83033451327d6f7066467e1fd
Timicanvas <adeyemishadrack@yahoo.com>
2024-07-30 23:28:28 +0100

    error hansled

--- static/js/script.js
+++ static/js/script.js
@@ -31,4 +31,6 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(response => response.json())
         .then(data => {
             if (data.error) {
+                addMessageToChat(data.error, "bot-message");
+            } else {
                 addMessage
